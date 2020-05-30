34b755589b4e935ac4443b424408d30a5fe15730
Matias <carossomatias@gmail.com>
2020-05-30 19:12:27 -0300

    change code

--- get-creds.py
+++ get-creds.py
@@ -1,49 +1,63 @@
 import boto3
 import json, webbrowser
 
-client_name= 'fplaptop'
-client_type= 'public'
-start_url= 'https://cloud-uala.awsapps.com/start#/'
+
+
 
 client = boto3.client('sso-oidc')
 
-response_client_registration = client.register_client(
-    clientName= client_name,
-    clientType= client_type,
-)
+def device_registration(client_name, client_type):
+    try:
+        response_client_registration = client.register_client(
+            clientName= client_name,
+            clientType= client_type,
+        )
+        return response_client_registration['clientId'], response_client_registration['clientSecret']
+    except Exception as e:
+        return e
 
-# print (response_client_registration)
-# print ('**************************')
-# jsonarray = json.dumps(response_client_registration)
+def get_auth_device(id, secret, start_url='https://cloud-uala.awsapps.com/start#/'):
+    try:
+        response_device_authorization = client.start_device_authorization(
+            clientId=id,
+            clientSecret=secret,
+            startUrl=start_url
+        )
+        return response_device_authorization['verificationUriComplete'], response_device_authorization['deviceCode'], response_device_authorization['userCode']
+    except Exception as e:
+        return e
 
-# print (jsonarray)
+def get_token(id, secret, device_code, user_code):
+    try:
+        response_token_creation = client.create_token(
+            clientId= id,
+            clientSecret= secret,
+            grantType='urn:ietf:params:oauth:grant-type:device_code', #review
+            deviceCode= device_code,
+            code= user_code
+        )
+        return response_token_creation
+    except Exception as e:
+        return e
 
-# with open('data.json', 'w') as f:
-#     json.dump(response_client_registration, f)
 
-response_device_authorization = client.start_device_authorization(
-    clientId= response_client_registration['clientId'],
-    clientSecret= response_client_registration['clientSecret'],
-    startUrl= start_url
-)
 
-url = response_device_authorization['verificationUriComplete']
 
-webbrowser.open_new(url)
-print ("Please manual login: %s \n" % (url))
+
+clientId, clientSecrets = device_registration('fplaptop', 'public')
+url, deviceCode, userCode = get_auth_device(clientId, clientSecrets)
+
+try:
+    webbrowser.open(url)
+except:
+    print("Please manual login: %s \n" % (url))
 
 input("After login, press Enter to continue...")
 
+response = get_token(clientId, clientSecrets, deviceCode, userCode)
 
-try:
-    response_token_creation = client.create_token(
-        clientId= response_client_registration['clientId'],
-        clientSecret= response_client_registration['clientSecret'],
-        grantType='urn:ietf:params:oauth:grant-type:device_code', #review
-        deviceCode= response_device_authorization['deviceCode'],
-        code=response_device_authorization['userCode'],
-        #refreshToken='string',
-    )
-    print (response_token_creation)
-except Exception as e:
-    print (e)
+print (response)
+
+
+
+
